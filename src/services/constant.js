e14d66d10c1c309b1510c9d860262824e5f794cf
perfAct-Flip <sohamsinha208@gmail.com>
2025-06-05 01:40:56 +0530

    changed from locally to production

--- src/services/constant.js
+++ src/services/constant.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = "https://acm-dashboard-backend.vercel.app/api/v1"; //to run on production
-export const BASE_URL = "http://localhost:5000/api/v1"; //to run locally
+export const BASE_URL = "https://acm-dashboard-backend.vercel.app/api/v1"; //to run on production
+// export const BASE_URL = "http://localhost:5000/api/v1"; //to run locally
 
 export const AUTH_ROUTE = `${BASE_URL}/auth`;
 export const FETCH_TEAM_ROUTE=`${BASE_URL}/teams/allTeam`
